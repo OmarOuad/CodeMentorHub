616ad1b2f79fa77e936a45e34813d8b1cfe96b4e
omar od <omar.ouadouha1@gmail.com>
2023-07-09 20:13:28 -0400

    Updated many files

--- src/Nav.js
+++ src/Nav.js
@@ -4,15 +4,17 @@ const Navbar = () => {
   const title = "CodeMentorHub";
   return (
     <nav className="navbar">
-      <Link to='/home'>
+      <Link to='/CodeMentorHub/home'>
         <h1>{title}</h1>
       </Link>
       
       <div className="links">
           <Link to='/CodeMentorHub/home'>Home</Link>
           <Link to='/CodeMentorHub/courses'>Courses</Link>
+          <Link to='/CodeMentorHub/tests'>Tests</Link>
           <Link to='/CodeMentorHub/ourteams'>Our teams</Link>
           <Link to='/CodeMentorHub/discussions'>Discussions</Link>
+          
     
 
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./Home";
 import Discussions from "./Discussions";
+import Ourteams from "./Ourteams"
+import Test from "./Test"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -15,6 +17,10 @@ root.render(
             <Route path='/CodeMentorHub/home' element={<Home/>}/>
             <Route path='/CodeMentorHub/courses' element={<App/>}/>
             <Route path='/CodeMentorHub/discussions' element={<Discussions/>}/>
+            <Route path='/CodeMentorHub/ourteams' element={<Ourteams/>}/>
+            <Route path='/CodeMentorHub/tests' element={<Test/>}/>
+
+
        </Routes>
     </Router>
  
